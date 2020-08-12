6d88f1d37d30ab1ce25aebf72576d3e03262a35f
Yoseph <yosephmariowibowo@gmail.com>
2020-08-12 22:36:57 +0700

    upload tugas

---
+++ script.js
@@ -0,0 +1,46 @@
+function changePageTo(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
+
+function filterTablekaryawan(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#myTableKaryawan tbody").rows;
+    
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }      
+    }
+    document.querySelector('#myInputKaryawan').addEventListener('keyup', filterTable, false);
+}
+
+function filterTabledivisi(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#myTableDivisi tbody").rows;
+    
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }      
+    }
+    document.querySelector('#myInputDivisi').addEventListener('keyup', filterTable, false);
+}
